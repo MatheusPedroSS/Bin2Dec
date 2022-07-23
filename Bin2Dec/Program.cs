618f0b996fc18b21ae9874f33d9b486d4b16d424
mathe <mathe@PEDRO>
2022-07-23 15:11:26 -0300

    Adicionar arquivos de projeto.

---
+++ Bin2Dec/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bin2Dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número Binario: ");
+            string numBinario = Console.ReadLine();
+            Console.WriteLine($"{Environment.NewLine}O decimal do binario digitado é: {parseBinToDec(numBinario)}");
+        }
+
+        static int parseBinToDec(string binario)
+        {
+            int atual = 0;
+            int aux = 0;
+            do
+            {
+                atual = (atual * 2) + int.Parse(binario[aux].ToString());
+                aux++;
+            } while (aux < binario.Length);
+
+            return atual;
+        }
+
+    }
+}
